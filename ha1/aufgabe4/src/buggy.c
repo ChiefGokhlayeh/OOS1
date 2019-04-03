735c4328b04848fa1b23983e1731a660e124c2dd
Andreas Baulig <andreas.baulig@gmail.com>
2019-04-03 09:59:10 +0200

    Add solution for ha1/aufgabe4

--- ha1/aufgabe4/src/buggy.c
+++ ha1/aufgabe4/src/buggy.c
@@ -1,29 +1,28 @@
 // Vergleich von Deklaration, Enumeration und Typumwandlung
 // in C und C++
 
-// Fehler                                               C    |  C++
-enum enumeration { 1st, 2nd, 3rd, 4th };     // Z1           |
-enum marks { '1', '2', '3', '4', '5', '6' }; // Z2           |
-enum class1 { DIETER, HANS, PETER, KAI };    // Z3           |
-enum class2 { ANDREA, KAI, SABINE, ZOE };    // Z4           |
-enum sports { FUSSBALL, BASKETBALL, HOCKEY };// Z5           |
+// Fehler                                           C
+//enum enumeration { 1st, 2nd, 3rd, 4th };     // Z1  invalid suffix
+//enum marks { '1', '2', '3', '4', '5', '6' }; // Z2  unexpected identifier
+enum class1 { DIETER, HANS, PETER, KAI };    // Z3  OK
+//enum class2 { ANDREA, KAI, SABINE, ZOE };    // Z4  OK
+enum sports { FUSSBALL, BASKETBALL, HOCKEY };// Z5  OK
 
-int main(void)                               // Z7           |
+int main(void)                               // Z7  OK
 {
-	int i;                                   // Z8           |
-	sports mysport;                          // Z9           |
-	enum sports mysport;                     // Z10          |
-	mysport = FUSSBALL;                      // Z11          |
-	mysport = FUSSBALL + 1;                  // Z12          |
-	mysport = 1;                             // Z13          |
-	mysport = 10;                            // Z14          |
-	mysport = mysport + 1;                   // Z15          |
-	mysport = sports(mysport + 1);           // Z16          |
-	float f;                                 // Z17          |
-	for (i = FUSSBALL; i <= HOCKEY; i++)     // Z18          |
-		mysport = sports(i);                 // Z19          |
-	for (int k = FUSSBALL; k <= HOCKEY; k++) // Z20          |
-		mysport = sports(k);                 // Z21          |
-	return 0;                                // Z22          |
+    int i;                                   // Z8  OK
+//  sports mysport;                          // Z9  unkown type (use enum)
+    enum sports mysport;                     // Z10 OK
+    mysport = FUSSBALL;                      // Z11 OK
+    mysport = FUSSBALL + 1;                  // Z12 OK
+    mysport = 1;                             // Z13 OK
+    mysport = 10;                            // Z14 OK
+    mysport = mysport + 1;                   // Z15 OK
+    mysport = (enum sports)(mysport + 1);       // Z16 sport() undefined function (not a valid C-cast)
+    float f;                                 // Z17 OK
+    for (i = FUSSBALL; i <= HOCKEY; i++)     // Z18 OK
+        mysport = (enum sports)i;                 // Z19 sport() undefined function (not a valid C-cast)
+    for (int k = FUSSBALL; k <= HOCKEY; k++) // Z20 OK
+        mysport = (enum sports)k;                 // Z21 sport() undefined function (not a valid C-cast)
+    return 0;                                // Z22 OK
 }
-
--- ha1/aufgabe4/src/buggy.cc
+++ ha1/aufgabe4/src/buggy.cc
@@ -1,29 +1,28 @@
 // Vergleich von Deklaration, Enumeration und Typumwandlung 
 // in C und C++
 
-// Fehler                                               C    |  C++
-enum enumeration { 1st, 2nd, 3rd, 4th };     // Z1           | 
-enum marks { '1', '2', '3', '4', '5', '6' }; // Z2           | 
-enum class1 { DIETER, HANS, PETER, KAI };    // Z3           |
-enum class2 { ANDREA, KAI, SABINE, ZOE };    // Z4           | 
-enum sports { FUSSBALL, BASKETBALL, HOCKEY };// Z5           |
+// Fehler                                           C++
+//enum enumeration { 1st, 2nd, 3rd, 4th };     // Z1  expected identifier before numeric constant
+//enum marks { '1', '2', '3', '4', '5', '6' }; // Z2  expected identifier before '1', '2', ...
+enum class1 { DIETER, HANS, PETER, KAI };    // Z3  OK
+//enum class2 { ANDREA, KAI, SABINE, ZOE };    // Z4  redeclaration of 'KAI'
+enum sports { FUSSBALL, BASKETBALL, HOCKEY };// Z5  OK
 
-int main(void)                               // Z7           |
+int main(void)                               // Z7  OK
 {
-	int i;                                   // Z8           |
-	sports mysport;                          // Z9           | 
-	enum sports mysport;                     // Z10          | 
-	mysport = FUSSBALL;                      // Z11          |
-	mysport = FUSSBALL + 1;                  // Z12          |
-	mysport = 1;                             // Z13          |
-	mysport = 10;                            // Z14          |
-	mysport = mysport + 1;                   // Z15          |
-	mysport = sports(mysport + 1);           // Z16          | 
-	float f;                                 // Z17          | 
-	for (i = FUSSBALL; i <= HOCKEY; i++)     // Z18          |
-		mysport = sports(i);                 // Z19          | 
-	for (int k = FUSSBALL; k <= HOCKEY; k++) // Z20          | 
-		mysport = sports(k);                 // Z21          | 
-	return 0;                                // Z22          |
+    int i;                                   // Z8  OK
+    sports mysport;                          // Z9  OK
+//    enum sports mysport;                     // Z10 redeclaration of 'sports mysport'
+    mysport = FUSSBALL;                      // Z11 OK
+    mysport = FUSSBALL + 1;                  // Z12 invalid conversion from 'int' to 'sports' (gcc -fpermissive)
+    mysport = 1;                             // Z13 invalid conversion from 'int' to 'sports' (gcc -fpermissive)
+    mysport = 10;                            // Z14 invalid conversion from 'int' to 'sports' (gcc -fpermissive)
+    mysport = mysport + 1;                   // Z15 invalid conversion from 'int' to 'sports' (gcc -fpermissive)
+    mysport = sports(mysport + 1);           // Z16 OK
+    float f;                                 // Z17 OK
+    for (i = FUSSBALL; i <= HOCKEY; i++)     // Z18 OK
+        mysport = sports(i);                 // Z19 OK
+    for (int k = FUSSBALL; k <= HOCKEY; k++) // Z20 OK
+        mysport = sports(k);                 // Z21 OK
+    return 0;                                // Z22 OK
 }
-
